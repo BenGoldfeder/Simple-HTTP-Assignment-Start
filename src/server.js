f495cfa33f3f4c27a6ba5d5e6c177ce2daa995eb
BenGoldfeder <goldfedb@gmail.com>
2020-09-03 18:24:26 -0400

    fixed http and set up another simple web server

--- src/server.js
+++ src/server.js
@@ -1,13 +1,14 @@
 const http = require('http');
 const fs = require('fs'); //"file system", a built-in library
 
-const port = process.env.PORT || process.env.NODE_PORT || 3001;
+const port = process.env.PORT || process.env.NODE_PORT || 3000;
 const index = fs.readFileSync(`${__dirname}/../client/client.html`);
 
 const onRequest = (request, respose) => {
   console.log(request.url);
   response.writeHead(200, { 'Content-Type': 'text/html' });
     response.write(index);
+    response.end();
 };
 
 http.createServer(onRequest).listen(port);

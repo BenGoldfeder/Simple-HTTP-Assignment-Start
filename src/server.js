445ed1dc482203517a8c508eab704a491ae47e76
BenGoldfeder <goldfedb@gmail.com>
2020-09-09 00:04:03 -0400

    Updated simple http with other webpages

--- src/server.js
+++ src/server.js
@@ -1,15 +1,37 @@
 const http = require('http');
-const fs = require('fs'); //"file system", a built-in library
+const htmlHandler = require('./htmlResponses.js');
+const textHandler = require('./textResponses.js');
+const jsonHandler = require('./jsonResponses.js');
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
-const index = fs.readFileSync(`${__dirname}/../client/client.html`);
 
-const onRequest = (request, respose) => {
-  console.log(request.url);
-  response.writeHead(200, { 'Content-Type': 'text/html' });
-    response.write(index);
-    response.end();
+const onRequest = (request, response) => {
+    console.log(request.url);
+
+    switch (request.url) {
+        case '/':
+            htmlHandler.getIndex(request, response);
+            break;
+        case '/page2':
+            htmlHandler.getPage2(request, response);
+            break;
+        case '/hello':
+            textHandler.getHello(request, response);
+            break;
+        case '/time':
+            textHandler.getTime(request, response);
+            break;
+        case '/helloJSON':
+            jsonHandler.getHelloJSON(request, response);
+            break;
+        case '/timeJSON':
+            jsonHandler.getTimeJSON(request, response);
+            break;
+        default:
+            htmlHandler.getIndex(request, response);
+            break;
+    }
 };
 
 http.createServer(onRequest).listen(port);
-console.log(`Listening on ${port}`);
+console.log(`Listening on 127.0.0.1: ${port}`);
